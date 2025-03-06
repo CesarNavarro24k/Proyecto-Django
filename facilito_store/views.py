d8d98cdd53f614e0af797384ef9f753a8e4e3d28
CesarNavarro24k <138552856+CesarNavarro24k@users.noreply.github.com>
2025-03-06 10:37:15 -0500

    nuevos cambios

--- facilito_store/views.py
+++ facilito_store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"index.html", {
-    "message": "listado de productos",
+    "message": "Listado de productos",
     "title": "Productos",
     "products": [
         {"title" : "Playera", "price" : 5, "stock" : True},
