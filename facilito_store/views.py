32794b0c7fedf3c69db8cdff5b3bf5fe6cb0e09f
CesarNavarro24k <138552856+CesarNavarro24k@users.noreply.github.com>
2025-03-06 11:05:04 -0500

    mas código

--- facilito_store/views.py
+++ facilito_store/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.shortcuts import redirect
 def index(request):
     return render(request,"index.html", {
     "message": "Listado de productos",
@@ -13,3 +15,13 @@ def index(request):
     ]
 
     })
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username") #diccionario
+        password = request.POST.get("password")
+        user = authenticate(username = username, password = password)
+        if user:
+            login(request,user)
+            return redirect("index")
+    return render(request, "users/login.html")
