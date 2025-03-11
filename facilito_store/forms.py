373e23c9aa0cae7f190213625bb0881c6544e40c
CesarNavarro24k <138552856+CesarNavarro24k@users.noreply.github.com>
2025-03-11 13:04:41 -0500

    mas cambios

--- facilito_store/forms.py
+++ facilito_store/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.contrib.auth.models import User
 class RegisterForm(forms.Form):
     username = forms.CharField(required=True,
                             min_length=4, max_length=50,
@@ -25,3 +25,22 @@ class RegisterForm(forms.Form):
             # "placeholder": "Password"
         })
     )
+    password2 = forms.CharField(label="Repite la contraseña",
+                                required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    "class":"form-control"
+                                }))
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Este usuario ya esta disponible")
+        return username
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este correo ya esta disponible")
+        return email
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get("password2") != cleaned_data.get("password"):
+            self.add_error("password2","El password no coincide")
